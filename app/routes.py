0be06b8c9871af049dbc9a52bfe28bd46c1a6db2
Carlos Alberto Cardoso <carloalbertocardoso@gmail.com>
2022-10-21 15:04:56 -0300

    first commit

---
+++ app/routes.py
@@ -0,0 +1,14 @@
+from app import app
+from flask import render_template
+
+@app.route('/')
+
+@app.route('/index')
+def index():
+    nomedealguem = 'Carlinhos'
+    dados = {"profissao":"Maluco","canal":"Não tenho"}
+    return render_template('index.html',nome = nomedealguem, dados=dados)
+
+@app.route('/contato')
+def contato():
+    return render_template('contato.html')
