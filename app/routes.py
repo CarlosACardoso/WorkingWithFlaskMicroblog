0c7a6cb0ae0a0f5515eccacdb7d9fe7b6bfa8871
Carlos Alberto Cardoso <carloalbertocardoso@gmail.com>
2022-10-22 15:39:25 -0300

    Remoção do MongoDB

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 from app import app
 from flask import render_template
 from flask import request
-from pymongo import MongoClient
+#from pymongo import MongoClient
 
 @app.route('/')
 
@@ -22,9 +22,9 @@ def autenticar():
     usuario = request.form.get('usuario')
     senha = request.form.get('senha')
     dados = {"usuário": usuario, "senha":senha}
-    client = MongoClient("mongodb+srv://adminuser:adminuser@mygame.t19zp9a.mongodb.net/?retryWrites=true&w=majority")
-    colocardados = client.get_database("clientslandpage")
-    dicionario = colocardados.clients_landpage
-    dicionario.insert_one(dados)
+  #  client = MongoClient("mongodb+srv://adminuser:adminuser@mygame.t19zp9a.mongodb.net/?retryWrites=true&w=majority")
+  #  colocardados = client.get_database("clientslandpage")
+  #  dicionario = colocardados.clients_landpage
+  #  dicionario.insert_one(dados)
 
-    return "Seu cadastro foi realizado com sucesso!"
+    return f"Seu cadastro foi realizado com sucesso! {dados}"
